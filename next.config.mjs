a7ae34710da09fae1ac21ab1a76a5966d5d5b1dd
tlun0911 <tlun0911@gmail.com>
2024-07-30 20:15:34 -0400

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'picsum.photos',
-            port: '',
-            pathname: '/id/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'cloudflare-ipfs.com',
-            port: '',
-            pathname: '/ipfs/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'source.unsplash.com',
-            port: '',
-            pathname: '/random/400x400',
-          },
-          
-        ],
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/id/**",
       },
-    
+      {
+        protocol: "https",
+        hostname: "cloudflare-ipfs.com",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        port: "",
+        pathname: "/random/400x400",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
