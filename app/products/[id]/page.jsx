e37c3beadba991e43894d950457b2414ded9d724
tlun0911 <tlun0911@gmail.com>
2024-07-31 09:50:36 -0400

    Updated to 30

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -21,7 +21,7 @@ function getRandomDateWithinTwoWeeks() {
 
 export async function generateStaticParams() {
  // const productIds = await getAllProductIds();
- const productIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+ const productIds = Array.from({ length: 30 }, (_, index) => index + 1);
   return productIds.map((id) => ({
     id: id.toString(),
   }));
